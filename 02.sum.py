44e542254e0d1f67c81f2e83d95bcc5a8ef70f9c
Nevil-patel0108 <nevu0189@gmail.com>
2026-05-08 20:41:18 +0530

    Add a simple sum program in Python

---
+++ 02.sum.py
@@ -0,0 +1,4 @@
+a = input("Enter a number: ")# This line prompts the user to enter a number and stores it in the variable 'a'.
+b = input("Enter a number: ")# This line prompts the user to enter a number and stores it in the variable 'b'.
+c = int(a) + int(b)# This line converts the strings to integers and adds them together.
+print("sum is", c)# This line prints the result.
