b263774b2d268c2632e17fd18a90c36072afd4a9
Nevil-patel0108 <nevu0189@gmail.com>
2026-05-10 13:07:35 +0530

    Add type checking for various variable types

---
+++ 03.type.py
@@ -0,0 +1,15 @@
+x=5#assigning an integer value to x
+type(x)#to check the type of x
+print('x is:', type(x))#to print the type of x
+
+y=5.7#assigning a float value to y
+type(y)#to check the type of y
+print('y is:', type(y))#to print the type of y
+
+z=True#assigning a boolean value to z
+type(z)#to check the type of z
+print('z is:', type(z))#to print the type of z
+
+a='Hello'#assigning a string value to a
+type(a)#to check the type of a
+print('a is:', type(a))#to print the type of a
