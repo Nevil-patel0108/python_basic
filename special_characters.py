f3d9827df9be8c7a66c75df46c3ee250169e984d
Nevil-patel0108 <nevu0189@gmail.com>
2026-06-02 14:32:38 +0530

    Add examples of special characters in print statements

---
+++ special_characters.py
@@ -0,0 +1,13 @@
+print("hello\n world")   # \n = New Line (moves text to the next line)
+
+print("hello\t world")   # \t = Tab Space (adds a horizontal tab)
+
+print("hello\b world")   # \b = Backspace (removes the character before it)
+
+print("hello\r world")   # \r = Carriage Return (moves cursor to the beginning of the line)
+
+print("hello\\ world")   # \\ = Prints a single backslash (\)
+
+print("hello\" world")   # \" = Prints a double quote (")
+
+print("hello\' world")   # \' = Prints a single quote (') 
