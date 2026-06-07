b1dceebafe6f0a8643defb4c41363262ea1d28ba
Nevil-patel0108 <nevu0189@gmail.com>
2026-06-07 12:21:40 +0530

    Implement variable deletion and reassignment example

---
+++ 05.delete.py
@@ -0,0 +1,7 @@
+x = 5                      # Variable x is assigned the value 5
+print('x is:', x)          # Prints the value of x
+
+y = 5                      # Variable y is assigned the value 5
+del y                      # Deletes the variable y from memory
+y = 10                     # Recreates y and assigns the value 10
+print('y is:', y)          # Prints the new value of y
